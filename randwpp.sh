abe549f6ef16e90fa8176c6eafde603b8b343cfb
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-11-24 12:46:27 -0700

    made wallpaper scripts more customizable

--- randwpp.sh
+++ randwpp.sh
@@ -2,10 +2,10 @@
 
 if [ -z $1 ]
 then
-	WPP_DIR=/home/felix/Pictures/wallpapers/
+	DIR=$WPP_DIR
 else
-	WPP_DIR=$1
+	DIR=$1
 fi
 
-IMAGE=$(ls $WPP_DIR | sort -R | tail -n 1)
-gsettings set org.gnome.desktop.background picture-uri file:///$WPP_DIR/$IMAGE
+IMAGE=$(ls $DIR | sort -R | tail -n 1)
+gsettings set org.gnome.desktop.background picture-uri file:///$DIR/"$IMAGE"
--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+re='^[0-9]+$'
+
+DIR=$WPP_DIR
+DELAY=120
+
+if [[ $1 =~ $re ]]
+then
+	DELAY=$1
+elif [ -z $2 ]
+then
+	DIR=$1
+else
+	DIR=$1
+	DELAY=$2
+fi
+
 while true
 do
-	randwpp $1
-	sleep $2
+	randwpp $DIR
+	sleep $DELAY
 done
